d9366c152277b53ccc819386da0174a10086cebf
joe66366 <90079294+joe66366@users.noreply.github.com>
2023-04-12 14:57:51 +0800

    Update Instant_One_User.js

--- web/frontEnd/src/js/Instant_One_User.js
+++ web/frontEnd/src/js/Instant_One_User.js
@@ -256,6 +256,14 @@ async function savefile(src,name){
   
   }
 
+function readTxt(file) {
+  fetch(file).then(function(response) {
+  response.text().then(function(text) {
+		return text;
+  });
+});
+}
+
 function do_cutcloth(data){
 	const video = document.getElementById('demo-video');
 	var canvas2 = document.getElementById('myCanvas2');
